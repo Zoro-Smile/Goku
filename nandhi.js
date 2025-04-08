3b49805e99c136b0f2a56b9e9be1e0eb0543f1c8
Zoro-Smile <nagaprakashudhaya2022@gmail.com>
2025-04-08 11:51:02 +0530

    this is updation on today

--- nandhi.js
+++ nandhi.js
@@ -1,56 +1,93 @@
-let propose = new Promise((accept,reject)=> {
-    let proposal = false
-    if (proposal)
-        accept("kindness")
-    else
-        reject("ego")
+// let propose = new Promise((accept,reject)=> {
+//     let proposal = false
+//     if (proposal)
+//         accept("kindness")
+//     else
+//         reject("ego")
+// })
+// propose.then((kind)=> {console.log(`she is loving me because of ${kind}`);
+// }).catch((ego)=>{console.log(`she is reject me because of my ${ego}`);
+// })
+
+// function cointoss(){
+//     return new Promise((Head,Tail)=>{
+//         var tossed = Math.floor(Math.random() *2)
+//         if (tossed==0)
+//             Head("head")
+//         else
+//             Tail("tail")
+//     })}
+
+// cointoss().then((head)=>{
+//     console.log(`Hey there! you won because of you have ${head}`);
+// }).catch((tail)=>{
+//     console.log(`Hey there! you lose because of you have ${tail}`);    
+// })
+
+
+// const reachA = new Promise((resolve,reject)=>{
+//     let reached = false
+//     if (reached==true)
+//         setTimeout(resolve,1000,"nandis is reached..")
+//     else
+//         reject("Nandhis is not reached..")
+// })
+
+// const reachB = new Promise((resolve,reject)=>{
+//     let reached = true
+//     if (reached==true)
+//         setTimeout(resolve,2000,"Nagaprakash is reached..")
+//     else
+//         reject("Nagaprakash is not reached..")
+// })
+
+// const reachC = new Promise((resolve,reject)=>{
+//     let reached = false
+//     if (reached==true)
+//         setTimeout(resolve,3000,"Sabarinathan is reached..")
+//     else
+//         reject("Sabarinathan is not reached..")
+// })
+
+// Promise.allSettled([reachA,reachB,reachC]).then((message)=>{
+//     console.log(message);
+// }).catch((message)=>{
+//     console.log(message); 
+// })
+
+
+// async function naga() {
+//     return 'hello'
+// }
+
+// console.log(naga());
+
+// async function synch() {
+//     console.log("async function is used..");
+//     try{
+//     fun = await reachC
+//     console.log(fun);}
+//     catch(msg){
+//         console.log(msg)
+//     }   
+// }
+
+// synch()
+ 
+
+ fetch('https://official-joke-api.appspot.com/jokes/programming/random')
+.then((result)=>result.json())
+.then((results)=>{
+    try{
+    console.log(results[0].setup,results[0].punchline);
+    }
+    catch{
+        console.log("any of error");
+        
+    }
+    
 })
-propose.then((kind)=> {console.log(`she is loving me because of ${kind}`);
-}).catch((ego)=>{console.log(`she is reject me because of my ${ego}`);
-})
-
-function cointoss(){
-    return new Promise((Head,Tail)=>{
-        var tossed = Math.floor(Math.random() *2)
-        if (tossed==0)
-            Head("head")
-        else
-            Tail("tail")
-    })}
-
-cointoss().then((head)=>{
-    console.log(`Hey there! you won because of you have ${head}`);
-}).catch((tail)=>{
-    console.log(`Hey there! you lose because of you have ${tail}`);    
-})
-
-
-const reachA = new Promise((resolve,reject)=>{
-    let reached = false
-    if (reached==true)
-        setTimeout(resolve,1000,"nandis is reached..")
-    else
-        reject("Nandhis is not reached..")
-})
-
-const reachB = new Promise((resolve,reject)=>{
-    let reached = true
-    if (reached==true)
-        setTimeout(resolve,2000,"Nagaprakash is reached..")
-    else
-        reject("Nagaprakash is not reached..")
-})
-
-const reachC = new Promise((resolve,reject)=>{
-    let reached = false
-    if (reached==true)
-        setTimeout(resolve,3000,"Sabarinathan is reached..")
-    else
-        reject("Sabarinathan is not reached..")
-})
-
-Promise.allSettled([reachA,reachB,reachC]).then((message)=>{
-    console.log(message);
-}).catch((message)=>{
-    console.log(message); 
+.catch((err)=>{
+    console.log(err);
+    
 })
